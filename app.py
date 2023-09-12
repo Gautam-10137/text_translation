22cbc0696ea55da45d70494e5973fd0884643db7
Gautam-10137 <pahwagautam786@gmail.com>
2023-09-12 15:33:19 +0530

    updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from translate_text import translate_text  # Import your text translation function here
-
+import os
 app = Flask(__name__)
 
 @app.route("/translate-text", methods=["POST"])
@@ -22,4 +22,5 @@ def translate_text_route():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
